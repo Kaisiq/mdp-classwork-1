3c080eef489d1fef4f6430198cbf875904f3840c
kaisiq <dahudohu@gmail.com>
2023-11-09 09:42:00 +0200

    add .editorconfig and code tests

---
+++ src/app_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from app import app
+
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_hello_world(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
