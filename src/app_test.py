02ac0998ef45341d4407be1249f9826ac1f3c541
kaisiq <dahudohu@gmail.com>
2023-11-11 01:14:38 +0200

    Edit tests file

--- src/app_test.py
+++ src/app_test.py
@@ -6,8 +6,8 @@ from . import app
 class TestApp(unittest.TestCase):
     def test_number_one(self):
         with app.test_client() as client:
-        rv = client.get('/')
-        self.assertEqual(response.status_code, 200)
+            rv = client.get('/')
+            self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
