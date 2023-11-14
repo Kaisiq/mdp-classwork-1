4532dd7d7a40b86d1f427a9e2c12a4946f866eeb
kaisiq <dahudohu@gmail.com>
2023-11-14 14:30:50 +0200

    Fix security issue (csrf)

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 import socket
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
 
 
 @app.route('/')
--- src/app_test.py
+++ src/app_test.py
@@ -1,7 +1,10 @@
 import unittest
 import socket
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
 
 
 @app.route('/')
